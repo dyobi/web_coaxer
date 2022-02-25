f8e8e207efb6fee2477c177638d1a9cbf277ea84
Dyobi <dyobi.92@gmail.com>
2022-02-25 19:47:13 +0900

    .

--- client/src/components/unit/landing/terms/index.jsx
+++ client/src/components/unit/landing/terms/index.jsx
@@ -1,6 +1,17 @@
 import { useSelector } from 'react-redux';
-import { BiXCircle } from 'react-icons/bi';
+import { BiChevronLeftCircle } from 'react-icons/bi';
 import { GiHeartBeats } from 'react-icons/gi';
+
+import {
+	TermsEn,
+	TermsKo,
+	PrivacyPolicyEn,
+	PrivacyPolicyKo,
+	CookiePolicyEn,
+	CookiePolicyKo,
+	OAuthPolicyEn,
+	OAuthPolicyKo
+} from './policy';
 
 import '../index.css';
 
@@ -11,20 +22,48 @@ const Component = ({ show, back }) => {
 	return (
 		<div className={`modal_container ${show ? 'show' : ''}`} onClick={() => back()}>
 			<div className="modal" onClick={(e) => e.stopPropagation()}>
-				<BiXCircle className='close_btn' onClick={() => back()} />
+				<BiChevronLeftCircle className='back_btn' onClick={() => back()} />
 				<GiHeartBeats className='logo_icon' />
 				<h3>{_ui.lang === 'en_US' ? 'Terms & Conditions' : '이용 약관'}</h3>
-				<span>
-					{_ui.lang === 'en_US' ?
-						<>
-							By clicking Log In, you agree to our <span>Terms</span>. Learn how we process your data in our <span>Privacy Policy</span> and <span>Cookie Policy</span>.
-						</>
-						:
-						<>
-							로그인하면 Coaxer <span>이용약관</span>에 동의하는 것으로 간주됩니다. Coaxer의 회원 정보 처리 방식은 <span>개인정보 처리방침</span> 및 <span>쿠키 정책</span>에서 확인해 보세요.
-						</>
-					}
-				</span>
+				{_ui.lang === 'en_US' ?
+					<div className='terms_container'>
+						<div className='terms'>
+							<p>Terms of Use Agreement</p>
+							<TermsEn />
+						</div>
+						<div className='terms'>
+							<p>Privacy Policy</p>
+							<PrivacyPolicyEn />
+						</div>
+						<div className='terms'>
+							<p>Cookie Policy</p>
+							<CookiePolicyEn />
+						</div>
+						<div className='terms'>
+							<p>What is OAuth?</p>
+							<OAuthPolicyEn />
+						</div>
+					</div>
+					:
+					<div className='terms_container'>
+						<div className='terms'>
+							<p>이용약관</p>
+							<TermsKo />
+						</div>
+						<div className='terms'>
+							<p>개인정보 처리방침</p>
+							<PrivacyPolicyKo />
+						</div>
+						<div className='terms'>
+							<p>쿠키 정책</p>
+							<CookiePolicyKo />
+						</div>
+						<div className='terms'>
+							<p>OAuth란?</p>
+							<OAuthPolicyKo />
+						</div>
+					</div>
+				}
 			</div>
 		</div>
 	);
---
+++ client/src/components/unit/landing/terms/policy/index.jsx
@@ -0,0 +1,77 @@
+export const TermsEn = () => {
+	return (
+		<>
+			By creating a Coaxer account, whether through a mobile device, mobile application or computer (collectively, the “Service”) you agree to be bound by (i) these Terms of Use, (ii) our Privacy Policy, Cookie Policy, Arbitration Procedures (if applicable to you), Safety Tips, and Community Guidelines, each of which is incorporated by reference into this Agreement, and (iii) any terms disclosed and agreed to by you if you purchase additional features, products or services we offer on the Service (collectively, this "Agreement"). If you do not accept and agree to be bound by all of the terms of this Agreement, please do not use the Service.<br /><br />
+			We may make changes to this Agreement and to the Service from time to time. We may do this for a variety of reasons including to reflect changes in or requirements of the law, new features, or changes in business practices. The most recent version of this Agreement will be posted at ordinary times, and you should regularly check for the most recent version. The most recent version is the version that applies. If the changes include material changes to your rights or obligations, we will notify you at least 30 days in advance of the changes (unless we’re unable to do so under applicable law) by reasonable means, which could include notification through the Service or via email. If you continue to use the Service after the changes become effective, then you agree to the revised Agreement.
+		</>
+	);
+};
+
+export const TermsKo = () => {
+	return (
+		<>
+			모바일 기기, 모바일 앱 또는 컴퓨터('서비스'라 통칭함)를 통해 콕서 계정을 생성함으로써, 귀하는 (i) 본 이용약관, (ii) 당사의 개인정보 취급방침, 쿠키 정책, 조정 절차(적용되는 경우), 안전을 위한 팁, 커뮤니티 가이드라인(각각 참조 목적으로 본 계약에 구체화되어 있음) 그리고 (iii) 서비스에서 당사가 제안하는 추가 기능, 제품 또는 서비스를 귀하가 구매하는 경우에 귀하가 공개하고 동의한 일체의 조건(본 '계약'이라 통칭함)에 구속된다는 데 동의하는 것입니다. 본 계약의 모든 조건을 수락하지 않고, 그러한 모든 조건에 구속된다는 데 동의하지 않는 경우에는 서비스를 이용하지 마십시오.<br /><br />
+			당사는 수시로 본 계약과 서비스를 변경할 수 있습니다. 당사는 법률의 변경 또는 법률 요건의 변경, 새로운 기능, 또는 사업 관행의 변경을 반영하기 위한 목적을 포함하여 다양한 이유로 본 계약과 서비스를 변경할 수 있습니다. 본 계약의 최신 버전은 상시 업데이트 되므로, 귀하는 최신 버전을 정기적으로 확인하여야 합니다. 최신 버전이라 함은 적용되는 버전을 뜻합니다. 이러한 변경에 귀하의 권리 또는 의무에 관한 상당한 변경이 포함되는 경우, 당사는 (법령에 의하여 통지를 할 수 없는 경우가 아닌 한) 적어도 30일 전에 합리적인 수단을 통해 그러한 변경을 통지하며, 이러한 수단에는 서비스 또는 이메일을 통한 통지가 포함될 수 있습니다. 변경이 발효된 후에도 귀하가 서비스를 계속하여 이용하는 경우, 귀하는 개정된 계약에 동의하는 것으로 간주됩니다.
+		</>
+	);
+};
+
+export const PrivacyPolicyEn = () => {
+	return (
+		<>
+			At Coaxer, your privacy is a top priority. Your privacy is at the core of the way we design and build the services and products you know and love, so that you can fully trust them and focus on building meaningful connections.<br /><br />
+			We appreciate that you put your trust in us when you provide us with your information and we do not take this lightly.<br /><br />
+			Our commitment to privacy. We design all of our products and services with your privacy in mind. We involve experts from various fields, including legal, security, engineering, product design and others to make sure that our decisions are taken with the utmost respect for your privacy.<br /><br />
+			Our commitment to transparency. Because we use many of the same online services you do, we know that insufficient information and overly complicated language are common issues in privacy policies. We take the exact opposite approach: we’re doing our best to write our Privacy Policy and related documents in plain language. We actually want you to read our policies and understand our privacy practices!<br /><br />
+			Our commitment to security. We have teams dedicated to keeping your data safe and secure. We constantly update our security practices and invest in our security efforts to enhance the safety of your information.
+		</>
+	);
+};
+
+export const PrivacyPolicyKo = () => {
+	return (
+		<>
+			Coaxer는 사용자의 개인정보 보호를 최우선으로 생각합니다. 사용자 개인정보 보호는 Coaxer의 서비스 및 제품의 설계와 구축 방향을 결정하는 핵심 가치로, 사용자가 안심하고 의미 있는 인연을 만들 수 있도록 최선을 다합니다.<br /><br />
+			Coaxer를 믿고 여러분의 개인정보를 공유해 주셔서 진심으로 감사드립니다. 제공된 정보는 모두 철저한 보안을 통해 관리됩니다.<br /><br />
+			개인정보 보호에 대한 약속. Coaxer는 사용자의 개인정보 보호를 늘 염두에 두고 모든 제품과 서비스를 설계합니다. 항상 사용자의 개인정보 보호를 최우선으로 하는 결정을 내릴 수 있도록 법률, 보안, 엔지니어링, 제품 디자인을 포함한 다양한 분야의 전문가와 협력합니다.<br /><br />
+			투명성에 대한 약속. Coaxer도 여러분이 이용하는 다수의 동일한 온라인 서비스를 상당수 이용하기에, 불충분한 정보와 지나치게 어렵고 복잡한 용어가 개인정보 처리방침의 대표적인 문제임을 잘 알고 있습니다. 이에 Coaxer는 다른 온라인 서비스와는 달리, 개인정보 처리방침 및 관련 문서를 이해하기 쉬운 용어로 작성하여 사용자가 이를 읽고 관련 방침을 제대로 이해할 수 있도록 노력합니다.<br /><br />
+			보안에 대한 약속. Coaxer는 데이터 보안 전담팀을 따로 두어 사용자의 데이터를 안전하게 보관하기 위해 최선을 다합니다. 현행 보안 방침을 주기적으로 업데이트하고 여러분의 정보 보안을 개선하기 위한 노력에 투자합니다.
+		</>
+	);
+};
+
+export const CookiePolicyEn = () => {
+	return (
+		<>
+			Cookies are small text files that are sent to or accessed from your web browser or your device’s memory. A cookie typically contains the name of the domain (internet location) from which the cookie originated, the “lifetime” of the cookie (i.e., when it expires) and a randomly generated unique number or similar identifier. A cookie also may contain information about your device, such as user settings, browsing history and activities conducted while using our services.<br /><br />
+			Other technologies such as web beacons (also called pixel, tags or clear gifs), tracking URLs or software development kits (SDKs) are used for similar purposes as cookies. Web beacons are tiny graphics files that contain a unique identifier that enable us to recognize when someone has visited our service or opened an e-mail that we have sent them. Tracking URLs are custom generated links that help us understand where the traffic to our webpages comes from. SDKs are small pieces of code included in apps, which function like cookies and web beacons.
+		</>
+	);
+};
+
+export const CookiePolicyKo = () => {
+	return (
+		<>
+			쿠키는 사용자의 웹 브라우저 또는 기기의 메모리로 전송되거나, 또는 이들이 접근하는 작은 텍스트 파일을 말합니다. 쿠키는 일반적으로 해당 쿠키가 생성된 도메인(인터넷 위치) 이름, 쿠키의 ‘수명’(쿠키가 만료되는 시점), 무작위로 생성된 고유한 번호 또는 유사한 식별자를 포함합니다. 또한 사용자 설정, 검색 기록, 서비스를 이용하는 동안 수행한 활동 등 사용자 기기에 관한 정보를 포함할 수 있습니다.<br /><br />
+			다른 온라인 서비스 제공업체와 마찬가지로, 당사는 사용자의 기본 설정을 기억하고, 당사 웹사이트 방문 시 사용자를 인식하며, 마케팅 캠페인 결과를 측정하고, 사용자 관심사에 맞는 맞춤형 광고를 제공하는 등 당사의 서비스를 제공, 보호, 개선하기 위해 쿠키를 사용합니다. 이를 위해 쿠키에서 얻은 정보를 당사가 보관 중인 기타 개인정보에 연결할 수도 있습니다.
+		</>
+	);
+};
+
+export const OAuthPolicyEn = () => {
+	return (
+		<>
+			OAuth is an open standard for authorization that enables client applications to access server resources on behalf of a specific Resource Owner. OAuth also enables Resource Owners (end users) to authorize limited third-party access to their server resources without sharing their credentials. For example, a Gmail user could allow LinkedIn or Flickr to have access to their list of contacts without sharing their Gmail username and password.<br /><br />
+			It can be used as an Authorization Server and as a Resource Server. An Authorization Server issues tokens to client applications on behalf of a Resource Owner for use in authenticating subsequent API calls to the Resource Server. The Resource Server hosts the protected resources, and can accept or respond to protected resource requests using access tokens.
+		</>
+	);
+};
+
+export const OAuthPolicyKo = () => {
+	return (
+		<>
+			OAuth는 클라이언트 응용 프로그램이 특정 리소스 소유자를 대신하여 서버 리소스에 액세스할 수 있도록 하는 권한 부여의 개방형 표준입니다. 또한 OAuth를 통해 리소스 소유자(최종 사용자)는 자격 증명을 공유하지 않고도 서버 리소스에 대한 제한된 타사 액세스를 허가할 수 있습니다. 예를 들어, 구글 사용자는 다른 플랫폼에서 사용자의 이름과 비밀번호를 공유하지 않고도 연락처 목록에 접근할 수 있도록 허용할 수 있습니다.<br /><br />
+			권한 부여 서버 및 리소스 서버로 사용할 수 있습니다. 권한 부여 서버는 리소스 서버에 대한 후속 API 호출을 인증하는 데 사용할 리소스 소유자 대신 클라이언트 응용 프로그램에 토큰을 발급합니다. 리소스 서버는 보호된 리소스를 호스팅하며 액세스 토큰을 사용하여 보호된 리소스 요청을 수락하거나 응답할 수 있습니다.
+		</>
+	);
+};
