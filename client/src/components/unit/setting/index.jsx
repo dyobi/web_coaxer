5653f4ce18df56883f010ca3a568e5385ac41127
Dyobi <dyobi.92@gmail.com>
2022-03-08 20:43:39 +0900

    .

--- client/src/components/unit/setting/index.jsx
+++ client/src/components/unit/setting/index.jsx
@@ -1,30 +1,16 @@
-import { useSelector } from 'react-redux';
-
 import Theme from './theme';
 import Profile from './profile';
 
 import './index.css';
 
 const Component = () => {
-
-	const _ui = useSelector(state => state.ui);
-
 	return (
 		<div className='setting_container'>
 			<div className='theme_container'>
 				<Theme />
 			</div>
 			<div className='profile_container'>
-				<div className='profile_container_title'>
-					{_ui.lang === 'en_US' ?
-						'Profile Settings'
-						:
-						'프로필 설정'
-					}
-				</div>
-				<div className='profile_settings'>
-					<Profile />
-				</div>
+				<Profile />
 			</div>
 		</div>
 	);
